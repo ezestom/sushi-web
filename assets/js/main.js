9ae128bd7c3c202204030eb780f705d81c80aa7f
ezestom <ezequielstom@gmail.com>
2022-11-08 23:02:22 -0300

    primer push

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+	navToggle = document.getElementById("nav-toggle"),
+	navClose = document.getElementById("nav-close");
+
+//? Menu Show
+/* Validate if constant exists */
+if (navToggle) {
+	navToggle.addEventListener("click", () => {
+		navMenu.classList.add("show-menu");
+	});
+}
+
+//? Menu Hidden
+/* Validate if constant exists */
+if (navClose) {
+	navClose.addEventListener("click", () => {
+		navMenu.classList.remove("show-menu");
+	});
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+	const navMenu = document.getElementById("nav-menu");
+	//when we click on each nav__link, we remove the show-menu
+	navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+	const header = document.getElementById("header");
+	//When the scroll id greater than 50 viewport height, and scroll-header class
+	this.scrollY >= 50
+		? header.classList.add("bg-header")
+		: header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
